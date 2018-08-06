55b35ef938bcbc6edc470c881fd1b0d0ff50c97b
Leslie <leslieleigh@outlook.com>
2018-08-06 18:52:13 +0800

    Update

--- source/index.ts
+++ source/index.ts
@@ -3,109 +3,24 @@ import * as path from 'path';
 import mkdirp from 'mkdirp';
 import * as ts from 'typescript';
 
-class Spy {
-    static process(inputPath: string, outputPath: string) {
-        fs.readFile(inputPath, (err: NodeJS.ErrnoException, data: Buffer) => {
-            if (err) {
-                console.log(`Error reading file: ${err.message}`);
-            }
-            fs.writeFile(
-                outputPath,
-                new SourceConverter(data.toString()).print(),
-                (err: NodeJS.ErrnoException) => {
-                    if (err) {
-                        console.log(`Error writing file: ${err.message}`);
-                    }
-                });
-        });
-    }
-}
-
-class SourceConverter {
-    private _sourceFile: ts.SourceFile;
-
-    private _forEach(node: ts.Node, fx: (node: ts.Node) => void) {
-        fx(node);
-        ts.forEachChild(node, (childNode) => {
-            this._forEach(childNode, fx);
-        });
-    }
-
-    constructor(content: string) {
-        this._sourceFile = ts.createSourceFile("", content, ts.ScriptTarget.Latest);
-
-        this._forEach(this._sourceFile, (node: ts.Node) => {
-            if (ts.isClassDeclaration(node)) {
-                this._injectProperties(node as ts.ClassDeclaration);
-            }
-        });
-    }
-
-    print() {
-        const printer = ts.createPrinter({
-            newLine: ts.NewLineKind.LineFeed
-        });
-        return printer.printFile(this._sourceFile);
-    }
-
-    _injectProperties(classDeclaration: ts.ClassDeclaration) {
-        let nonPropertyMembers = new Set<string>();
-        for (let member of classDeclaration.members) {
-            if (member.name as ts.Identifier) {
-                nonPropertyMembers.add((member.name as ts.Identifier).text);
-            }
-        }
-
-        let thisMembers = new Set<string>();
-        this._forEach(classDeclaration, (node: ts.Node) => {
-            if (ts.isPropertyAccessExpression(node)) {
-                if (node.expression.kind == ts.SyntaxKind.ThisKeyword) {
-                    thisMembers.add(node.name.text);
-                }
-            }
-        });
-
-        let properties = [];
-        for (let thisMember of thisMembers) {
-            if (!nonPropertyMembers.has(thisMember)) {
-                properties.push(ts.createProperty(
-                    undefined,
-                    undefined,
-                    thisMember,
-                    undefined,
-                    ts.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
-                    undefined
-                ));
-            }
-        }
-        classDeclaration.members = ts.createNodeArray(
-            classDeclaration.members.concat(properties));
-    }
-}
-
 function main(inputDir: string, outputDir: string) {
     let rootDir = inputDir;
 
-    let forEachFile = (dir: string, fx: (file: string) => void) => {
-        fs.readdir(dir, (err, files) => {
-            if (err) {
-                return;
+    let forEachFile = (dir: string, extension: string, fx: (file: string) => void) => {
+        let entries = fs.readdirSync(dir);
+        for (let entry of entries) {
+            entry = path.resolve(dir, entry);
+            let stat = fs.statSync(entry);
+            if (stat && stat.isDirectory()) {
+                forEachFile(entry, extension, fx);
+            } else if (entry.endsWith(extension)) {
+                fx(path.relative(rootDir, entry));
             }
-            for (let file of files) {
-                file = path.resolve(dir, file);
-                fs.stat(file, (err, stat) => {
-                    if (stat && stat.isDirectory()) {
-                        forEachFile(file, fx);
-                    }
-                    else if (file.endsWith(".js")) {
-                        fx(path.relative(rootDir, file));
-                    }
-                });
-            }
-        });
+        }
     };
 
-    forEachFile(inputDir, (file) => {
+    let files: string[] = [];
+    forEachFile(inputDir, ".js", (file) => {
         let input = path.resolve(inputDir, file);
         let output = path.resolve(outputDir, file);
         output = output.replace(".js", ".ts");
@@ -115,8 +30,91 @@ function main(inputDir: string, outputDir: string) {
                 console.log(`Failed to create output directory ${outputParentDir}.`);
             }
         });
-        Spy.process(input, output);
+        files.push(output);
+        fs.copyFileSync(input, output);
     });
+
+    let program = ts.createProgram(files, {});
+    let typeChecker = program.getTypeChecker();
+
+    let sourceFiles = program.getSourceFiles();
+    for (let sourceFile of sourceFiles) {
+        _forEach(sourceFile, (node: ts.Node) => {
+            if (ts.isClassDeclaration(node)) {
+                _injectProperties(node as ts.ClassDeclaration, typeChecker);
+            }
+        });
+    }
+
+    console.log(`success`);
+}
+
+function _forEach(node: ts.Node, fx: (node: ts.Node) => void) {
+    fx(node);
+    ts.forEachChild(node, (childNode) => {
+        _forEach(childNode, fx);
+    });
+}
+
+function _injectProperties(classDeclaration: ts.ClassDeclaration, typeChecker: ts.TypeChecker) {
+    let nonPropertyMembers = new Set<string>();
+
+    let collectNonPropertyMembers = (classDecl: ts.ClassDeclaration) => {
+        for (let member of classDeclaration.members) {
+            if (member.name as ts.Identifier) {
+                nonPropertyMembers.add((member.name as ts.Identifier).text);
+            }
+        }
+
+        if (classDecl.heritageClauses) {
+            for (let heri of classDecl.heritageClauses) {
+                if (heri.token == ts.SyntaxKind.ExtendsKeyword) {
+                    for (let type of heri.types) {
+                        let base = typeChecker.getSymbolAtLocation(type.expression);
+                        if (base && base.valueDeclaration) {
+                            if (ts.isClassDeclaration(base.valueDeclaration)) {
+                                collectNonPropertyMembers(base.valueDeclaration);
+                            }
+                        } else {
+                            // DO IT: warnning?
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    collectNonPropertyMembers(classDeclaration);
+
+    let thisMembers = new Set<string>();
+    _forEach(classDeclaration, (node: ts.Node) => {
+        if (ts.isPropertyAccessExpression(node)) {
+            if (node.expression.kind == ts.SyntaxKind.ThisKeyword) {
+                thisMembers.add(node.name.text);
+            }
+        }
+    });
+
+    let properties = [];
+    let ps = [];
+    for (let thisMember of thisMembers) {
+        if (!nonPropertyMembers.has(thisMember)) {
+            ps.push(thisMember);
+            properties.push(ts.createProperty(
+                undefined,
+                undefined,
+                thisMember,
+                undefined,
+                ts.createKeywordTypeNode(ts.SyntaxKind.AnyKeyword),
+                undefined
+            ));
+        }
+    }
+    classDeclaration.members = ts.createNodeArray(
+        classDeclaration.members.concat(properties));
+
+    if (properties.length != 0)
+        console.log(`${classDeclaration.name ? classDeclaration.name.text : "<unnamed>"} - ${ps}`);
 }
 
 main(String.raw`E:\Repos\Cocos\engine-3d\lib`, String.raw`D:\out`);
